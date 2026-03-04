35edb8e249547000d89c969d6d3ce05f795aaba1
asiri-gayashan <gayashanx@gmail.com>
2026-03-04 12:57:09 +0530

    feat: implement AuthSync component and API integration for user authentication

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -3,6 +3,8 @@ import "../global.css";
 import { ClerkProvider } from "@clerk/clerk-expo";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { tokenCache } from '@clerk/clerk-expo/token-cache'
+import AuthSync from "@/components/AuthSync";
+import { StatusBar } from "expo-status-bar";
 
 const queryClient = new QueryClient();
 
@@ -10,6 +12,8 @@ export default function RootLayout() {
   return (
     <ClerkProvider tokenCache={tokenCache}>
       <QueryClientProvider client={queryClient} >
+        <AuthSync />
+        <StatusBar  style="light" />
         <Stack screenOptions={{ headerShown: false, contentStyle: { backgroundColor: "#0D0D0F" } }}>
            <Stack.Screen name="(auth)" options={{ animation: "fade" }} />
           <Stack.Screen name="(tabs)" options={{ animation: "fade" }} />
